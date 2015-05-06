22c082de53367418ad0bb0c3a669193ceceaa7fa
Mahmood Bagil <M.B.Mahmood@wlv.ac.uk>
2015-05-06 17:32:00 +0100

    Login
    MySQL Database Configration File

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$hostname='localhost';
+$username='username';
+$password='password';
+$database='database';
+
+$con=mysqli_connect($hostname,$username,$password,$database) or die(mysqli_error());
+
+?>
